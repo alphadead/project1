a43c14229a673cce21af946bc3a2075675a89d97
alphadead <adarshgupta1563@gmail.com>
2021-06-24 16:45:10 +0530

    sign up and register page, **issues

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:vamos/locator.dart';
+import 'package:vamos/loginPages/register.dart';
+import 'package:vamos/loginPages/signUp.dart';
 import 'package:vamos/testScreen.dart';
 
 void main() {
@@ -17,7 +19,7 @@ class MyApp extends StatelessWidget {
       designSize: Size(360, 690),
       builder: () => MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: TestScreen(),
+        home: RegisterPage(),
       ),
     );
   }
