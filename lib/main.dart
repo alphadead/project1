ec8ef3e18f83385a1b3e5b576e04104597b57f72
rishabh1405 <singh.rishabh30@gmail.com>
2021-06-24 13:09:33 +0530

    Device preview package added for testing

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,23 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:vamos/locator.dart';
 import 'package:vamos/testScreen.dart';
 
+// void main() {
+//   setupLocator();
+//   runApp(MyApp());
+// }
+
 void main() {
   setupLocator();
-  runApp(MyApp());
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
